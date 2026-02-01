a67fe0ee69df7bda38a06150748b6dbcceb49953
srojgam0907 <srojgam0907@g.educaand.es>
2026-02-01 13:03:50 +0100

    Funciones js para recibir tarea y crear tarea

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
 const boton= document.querySelector("#btnAgregar");
+const input= document.querySelector("#tareaInput");
+const tarea= document.querySelector("#listaTareas");
 
 
 boton.addEventListener("click", (event) => {
-    event.preventDefault(); //para que la pagina no se recargue
     
-    const input= document.querySelector("#tareaInput");
-    const tarea= document.querySelector("#listaTareas");
+    let texto= recibirTarea();
 
+    if(texto != undefined) { //si no hay tarea impide que se cree el espacio para la tarea
+        crearTarea(texto);
+    }
+    
 });
 
-//Añade una tarea a la lista cuando se le llama
-function añadirTarea() {
+//Recibe el texto que se introduce en el input y valida si está o no vacío.
+//Si es correcto la devuelve.
+function recibirTarea() {
+    let texto;
 
+        texto= input.value.trim();
+
+        if(texto.length === 0) { //Comprueba si el texto está en blanco, si lo está corta la validación
+            return;
+        }
+
+    return texto;
 }
 
 //Crea el elemento visual de una tarea
-function crearTarea() {
+function crearTarea(text) {
+    const elemento= document.createElement("li");
 
+    elemento.textContent= text;
+    tarea.appendChild(elemento);
 }
 
 //Marca la tarea como completada
