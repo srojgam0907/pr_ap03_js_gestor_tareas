d903f8c64222de19e211c68753e2f493d0101b5b
srojgam0907 <srojgam0907@g.educaand.es>
2026-02-01 12:24:06 +0100

    estructura basica de las funciones js

---
+++ script.js
@@ -0,0 +1,30 @@
+const boton= document.querySelector("#btnAgregar");
+
+
+boton.addEventListener("click", (event) => {
+    event.preventDefault(); //para que la pagina no se recargue
+    
+    const input= document.querySelector("#tareaInput");
+    const tarea= document.querySelector("#listaTareas");
+
+});
+
+//Añade una tarea a la lista cuando se le llama
+function añadirTarea() {
+
+}
+
+//Crea el elemento visual de una tarea
+function crearTarea() {
+
+}
+
+//Marca la tarea como completada
+function completado() {
+
+}
+
+//Se encarga de eliminar tareas de la lista
+function eliminarTareas() {
+
+}
